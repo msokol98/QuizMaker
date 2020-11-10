a2cf051252b3806a228e9e6c57e78ef10151331e
Mitchell Sokol <msokol@live.unc.edu>
2020-11-10 09:52:54 -0500

    using linode instead of aws

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,6 @@
-const desktop = "10.0.0.4";
+//const desktop = "10.0.0.4";
 // const aws_server = "3.136.106.200";
+const linode = "45.33.84.167"
 
-export const apiHost = process.env.NODE_ENV === "development" ? "http://localhost:8080" : `${desktop}:8080`;
-export const webHost = process.env.NODE_ENV === "development" ? "http://localhost:3000" : desktop;
+export const apiHost = process.env.NODE_ENV === "development" ? "http://localhost:8080" : `${linode}:8080`;
+export const webHost = process.env.NODE_ENV === "development" ? "http://localhost:3000" : linode;
